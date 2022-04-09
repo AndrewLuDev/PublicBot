01ca5c6637aca549e0149c278a9fe457126c4554
Andrew Lu <94149647+AndrewLuDev@users.noreply.github.com>
2022-04-09 19:22:28 +0000

    initial setup

---
+++ main.py
@@ -0,0 +1,14 @@
+import discord
+import os
+
+client = discord.Client()
+
+@client.event   
+async def on_ready():
+  print('We have logged in as {0.user}'.format(client))
+  
+  @client.event
+
+  async def on_message(message):
+    pass
+client.run(os.getenv('TOKEN'))
