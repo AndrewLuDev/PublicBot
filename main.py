e8b96cafd7bfdc2f889cd2459790cb08126b6141
Andrew Lu <94149647+AndrewLuDev@users.noreply.github.com>
2022-04-10 22:33:17 +0000

    Added ability to delete messages

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 import discord
 import os
 
+friendsIds = {}
+friendsList = []
+
+for userInfo in os.getenv('friendsList').split(", "):
+  #friendsIds format:
+  #name1: id1, name2: id2, name3: id3, etc.
+  userInfo = userInfo.split(': ')
+  friendsList.append(userInfo[0])
+  friendsIds[userInfo[0]] = int(userInfo[1])
+
+print(friendsIds)
+
 client = discord.Client()
 
 @client.event   
@@ -10,5 +22,21 @@ async def on_ready():
   @client.event
 
   async def on_message(message):
-    pass
+    msg = message.content
+    msgID = message.author.id
+    msgArgs = msg.split()
+
+    if msgID in friendsIds.values():
+      print(msgID)
+      if len(msgArgs) >= 2:
+
+        #deleting messages
+        if msg.startswith("!del "):
+          msgArgs[1] = int(msgArgs[1])
+          if msgArgs[1] >= 1 and msgArgs[1] <= 10:
+            await message.channel.purge(limit=msgArgs[1]+1)
+          else:
+            await message.channel.send('Please enter a number from 1 to 10')
+  
+
 client.run(os.getenv('TOKEN'))
